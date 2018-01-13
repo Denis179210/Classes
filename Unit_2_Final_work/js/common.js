c42d4090241f0de01d2d17fc04e6cfa0fbca64d1
Denis179210 <denis179210@gmail.com>
2018-01-13 15:57:47 +0200

    Add Unit_2_Final_work

---
+++ Unit_2_Final_work/js/common.js
@@ -0,0 +1,12 @@
+$('document').ready(function() {
+	console.log($('.pictures').isotope)
+	// $('.pictures').isotope({
+	// 	percentPosition: true,
+	// 	itemSelector: '.pictures-item',
+	// 	layoutMode: 'masonry',
+	// 	masonry: {
+ //    		columnWidth: 24,
+ //    		gutter: 0
+ // 		 }
+	// });
+})
