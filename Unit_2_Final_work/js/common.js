ee0351c7c519c515ff296f5c5fb4b7a998c06160
Denis179210 <denis179210@gmail.com>
2018-01-13 22:54:28 +0200

    Add Unit_2_Final_Work

--- Unit_2_Final_work/js/common.js
+++ Unit_2_Final_work/js/common.js
@@ -9,4 +9,32 @@ $('document').ready(function() {
  //    		gutter: 0
  // 		 }
 	// });
+	// 47.856446, 35.094205
+	function initMap() {
+        var uluru = {lat: 47.856446, lng: 35.094205};
+        var map = new google.maps.Map($('#map')[0], {
+          zoom: 17,
+          center: uluru
+        });
+        // var marker = new google.maps.Marker({
+        //   position: uluru,
+        //   map: map
+        // });
+      }
+
+    initMap();
+
+	$('.carosl_first').slick({
+		autoplay: true,
+		autoplaySpeed: 4000,
+		dots: true
+	});
+	$('.carosl_second').slick({
+		autoplay: true,
+		autoplaySpeed: 4000,
+		dots: true,
+		speed: 800,
+		cssEase: 'ease',
+		arrows: false,
+	})
 })
